a8a30a507ebb712a9c1adf04c6e88c35eba0033f
Pookoloco <90471681+Pookoloco@users.noreply.github.com>
2021-10-20 19:24:37 +0200

    Add files via upload

---
+++ Opdracht_06/script.js
@@ -0,0 +1,21 @@
+function Makepizza (){
+
+    console.log("Make dough");
+    console.log("Add sauce and toppings");
+    console.log("Bake until golden brown");
+    
+}
+
+function Makesushi (){
+
+    console.log("flatten salmon");
+    console.log("Add avacado and cream cheese");
+    console.log("Roll and cut into chuck sizes pieces")
+}
+
+
+Makesushi();
+Makesushi();
+Makesushi();
+Makepizza();
+Makepizza();
